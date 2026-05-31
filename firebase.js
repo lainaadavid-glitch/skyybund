b48f350d72024fb8d03a46281b6b2969fef033fe
lainaadavid-glitch <lainaadavid@gmail.com>
2026-05-31 18:21:39 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCealI_1It7_F_epQdSNEkfWUgLAK-ACX0",
+  apiKey: "...",
   authDomain: "skyybund.firebaseapp.com",
   projectId: "skyybund",
-  storageBucket: "skyybund.firebasestorage.app",
+  storageBucket: "skyybund.appspot.com",
   messagingSenderId: "789154861574",
-  appId: "1:789154861574:web:2dcfee93e36895a605a987",
-  measurementId: "G-1XY1Q33L74"
+  appId: "1:789154861574:web:2dcfee93e36895a605a987"
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Safe analytics (won’t break site)
-let analytics;
-if (typeof window !== "undefined") {
-  analytics = getAnalytics(app);
-}
-
-export default app;
+const analytics = getAnalytics(app);
+</script>
